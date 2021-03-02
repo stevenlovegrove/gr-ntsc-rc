dfee05f6e518e7142df81e7a3b1e8857ade4c49c
Steven Lovegrove <stevenlovegrove@gmail.com>
2021-03-01 21:42:04 -0800

    Updates for GNU Radio 3.8

--- cmake/Modules/FindCppUnit.cmake
+++ cmake/Modules/FindCppUnit.cmake
@@ -5,35 +5,35 @@
 # Find the CppUnit includes and library
 #
 # This module defines
-# CPPUNIT_INCLUDE_DIR, where to find tiff.h, etc.
-# CPPUNIT_LIBRARIES, the libraries to link against to use CppUnit.
-# CPPUNIT_FOUND, If false, do not try to use CppUnit.
+# cppunit_INCLUDE_DIR, where to find tiff.h, etc.
+# cppunit_LIBRARIES, the libraries to link against to use CppUnit.
+# cppunit_FOUND, If false, do not try to use CppUnit.
 
-INCLUDE(FindPkgConfig)
-PKG_CHECK_MODULES(PC_CPPUNIT "cppunit")
+find_package(PkgConfig QUIET)
+pkg_check_modules(PC_cppunit QUIET cppunit)
 
-FIND_PATH(CPPUNIT_INCLUDE_DIRS
+FIND_PATH(cppunit_INCLUDE_DIRS
     NAMES cppunit/TestCase.h
-    HINTS ${PC_CPPUNIT_INCLUDE_DIR}
+    HINTS ${PC_cppunit_INCLUDE_DIR}
     ${CMAKE_INSTALL_PREFIX}/include
     PATHS
     /usr/local/include
     /usr/include
 )
 
-FIND_LIBRARY(CPPUNIT_LIBRARIES
+FIND_LIBRARY(cppunit_LIBRARIES
     NAMES cppunit
-    HINTS ${PC_CPPUNIT_LIBDIR}
+    HINTS ${PC_cppunit_LIBDIR}
     ${CMAKE_INSTALL_PREFIX}/lib
     ${CMAKE_INSTALL_PREFIX}/lib64
     PATHS
-    ${CPPUNIT_INCLUDE_DIRS}/../lib
+    ${cppunit_INCLUDE_DIRS}/../lib
     /usr/local/lib
     /usr/lib
 )
 
-LIST(APPEND CPPUNIT_LIBRARIES ${CMAKE_DL_LIBS})
+LIST(APPEND cppunit_LIBRARIES ${CMAKE_DL_LIBS})
 
 INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CPPUNIT DEFAULT_MSG CPPUNIT_LIBRARIES CPPUNIT_INCLUDE_DIRS)
-MARK_AS_ADVANCED(CPPUNIT_LIBRARIES CPPUNIT_INCLUDE_DIRS)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(cppunit DEFAULT_MSG cppunit_LIBRARIES cppunit_INCLUDE_DIRS)
+MARK_AS_ADVANCED(cppunit_LIBRARIES cppunit_INCLUDE_DIRS)
